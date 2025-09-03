547372ed92e328d1ac9b2644fa1dbbfd8e497436
Yan Leido <leido1@mail.ru>
2025-09-03 23:58:17 +0300

    +my_db +django-extensions +six_records

--- ProjectApp/settings.py
+++ ProjectApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Voka',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- Voka/models.py
+++ Voka/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Services(models.Model):
+    title = models.CharField(max_length=60)
+    content = models.TextField(blank=True)
+    price = models.FloatField(null=True, blank=True)
+    availability = models.BooleanField(default=True)
